b424baa7a16070fd3f7872fa86fdb96dab93037b
rafae <rafae@10.146.192.131>
2025-04-07 10:50:16 -0300

    inicio do projeto

---
+++ src/main/java/com/mycompany/projetotriangulo/ProjetoTriangulo.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.projetotriangulo;
+
+/**
+ *
+ * @author rafae
+ */
+public class ProjetoTriangulo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/model/Triangulo.java
@@ -0,0 +1,40 @@
+
+package model;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rafae
+ */
+public class Triangulo {
+    private double base;
+    private double altura;
+    
+    public Triangulo(){
+        this(0,0);
+    }
+    public Triangulo(double base, double altura){
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double getArea(){
+        return ((getBase()*getAltura())/2.0);
+    }
+}
